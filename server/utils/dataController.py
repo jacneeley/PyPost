cefc5936583fd3ad6c95b6df79c3a01273b2a253
3ak <jakneeley@outlook.com>
2024-05-09 09:04:37 -0500

    fixed update function

--- server/utils/dataController.py
+++ server/utils/dataController.py
@@ -3,16 +3,14 @@ user_code: list = []
 
 #add or update
 def update(user_submission: dict) -> list:
-    if user_code:
-        for i in user_code:
-            if user_submission['username'] == i['username']:
-                print(user_submission['username'], i['username'])
-                print("user %s already exists\nupdating..." % i['username'])
-                return i.update(user_submission)
-            else:
-                return user_code.append(user_submission)            
-    else: 
-        return user_code.append(user_submission)
+    for i in user_code:
+        if user_submission['username'] == i['username']:
+            print("User %s already exists. Updating..." % i['username'])
+            i.update(user_submission)
+            return user_code
+    else: #else statement triggers if no match is found in the for loop
+        user_code.append(user_submission)
+        return user_code
     
 
 def displayAll() -> list:
@@ -22,4 +20,18 @@ def deleteById(id: int) -> None:
     del user_code[id]
 
 if __name__ == "__main__":
+    #tests
     print(displayAll())
+    d = {"username":"peter","code":"hi"}
+    update(d)
+    print(displayAll())
+    e = {"username":"peter","code":"print('hi')"}
+    update(e)
+    print(displayAll())
+    j = {"username":"3ak","code":"test"}
+    update(j)
+    print(displayAll())
+    k = {"username":"3ak","code":"print('test')"}
+    update(k)
+    print(displayAll())
+    
